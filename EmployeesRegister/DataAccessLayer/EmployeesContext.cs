0e6881a2a59f307767fd4c8fce3ce856c6e1fe2b
Aryo Pehlewan <aryopehlewan@hotmail.com>
2016-05-10 15:18:43 +0200

    DB created

---
+++ EmployeesRegister/DataAccessLayer/EmployeesContext.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeesRegister.DataAccessLayer
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class EmployeesContext : DbContext
+    {
+        public DbSet<Models.Employee> Employees { get; set; }
+
+        // Your context has been configured to use a 'EmployeesContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'EmployeesRegister.DataAccessLayer.EmployeesContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'EmployeesContext' 
+        // connection string in the application configuration file.
+        public EmployeesContext()
+            : base("DefaultConnection​​")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ EmployeesRegister/Migrations/Configuration.cs
@@ -0,0 +1,83 @@
+namespace EmployeesRegister.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EmployeesRegister.DataAccessLayer.EmployeesContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EmployeesRegister.DataAccessLayer.EmployeesContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            var employees = new List<Models.Employee>();
+
+            var positions = new string[]
+            {
+                "Worker",
+                "Teamleader",
+                "Manager",
+            };
+
+            var departments = new string[]
+            {
+                "Factory",
+                "Sales",
+                "Coporate",
+            };
+
+            var rnd = new Random(new System.DateTime().Millisecond);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var fn = RandomString(rnd, 10);
+                    var ln = RandomString(rnd, 10);
+                    var sl = rnd.Next(1, 10) * 1000;
+                    var ps = positions[rnd.Next(0, positions.Length - 1)];
+                    var dp = departments[rnd.Next(0, departments.Length - 1)];
+
+                    var employee = new Models.Employee
+                    {
+                        FirstName = fn,
+                        LastName = ln,
+                        Salary = sl,
+                        Position = ps,
+                        Department = dp,
+                    };
+
+                    employees.Add(employee);
+                }
+            }
+
+
+            context.Employees.AddOrUpdate(p => p.LastName, employees.ToArray());
+        }
+
+        private static string RandomString(Random random, int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
